66cb653c410cde22e98dec3528c1fad78af4fd2e
mustafizpi <mustafizur.rahman@pistrategy.org>
2018-07-30 17:05:36 +0600

    Implementing BottomNavigationView and NavigationDrawer to learn Kotlin syntax

--- app/src/main/java/com/idl/musta/kotlinlearning/SecondActivity.kt
+++ app/src/main/java/com/idl/musta/kotlinlearning/SecondActivity.kt
@@ -1,6 +1,10 @@
 package com.idl.musta.kotlinlearning
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 import java.util.*
@@ -14,7 +18,31 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
         generateRandomNumber()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_second, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_activity -> {
+                Toast.makeText(this, "Testing", Toast.LENGTH_SHORT).show()
+                val drawerIntent = Intent(this, DrawerActivity::class.java)
+                startActivity(drawerIntent)
+                return true
+            }
+
+            R.id.action_activity2 -> {
+                val bottomNavIntent = Intent(this, BottomNavActivity::class.java)
+                startActivity(bottomNavIntent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun generateRandomNumber() {
